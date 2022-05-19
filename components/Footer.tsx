a955431f064a0099a27b3183f405f9b4c126308f
Hyeonseung Kang <h5k@ajou.ac.kr>
2022-05-20 01:45:28 +0900

    icon, footer updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,6 +12,16 @@ const Footer = () => {
         <span className={styles.logo}>
           <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
         </span>
+        <span className={styles.logo}>
+          <a
+            href="https://github.com/khskeb0513/shuffle-alarm.git"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {' '}
+            @khskeb0513
+          </a>
+        </span>
       </a>
     </footer>
   );
